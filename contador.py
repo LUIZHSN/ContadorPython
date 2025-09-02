b13cb1b25d7dc88ae9488faefc554a2ba9740864
Luiz Henrique <57970582+LUIZHSN@users.noreply.github.com>
2025-09-02 08:11:26 -0400

    Create contador.py

---
+++ contador.py
@@ -0,0 +1,8 @@
+# Esse script conta de 1 a 10 e imprime cada número
+print("Iniciando a contagem...")
+
+# Loop para contar de 1 a 10
+for numero in range(1, 11):
+  print(numero)
+
+print("Contagem finalizada!")
